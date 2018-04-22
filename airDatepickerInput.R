c8d685564a12e6e55b79920fc0a3673724ed7137
pvictor <perrier.victor@gmail.com>
2018-04-22 13:31:30 +0200

    documentation

--- airDatepickerInput.R
+++ airDatepickerInput.R
@@ -11,6 +11,57 @@
 library(shiny)
 
 
+
+#' @title Air Date Picker Input
+#' 
+#' @description An alternative to \code{dateInput} to select single, multiple or date range. 
+#'  And two alias to select months or years.
+#'
+#' @param inputId The \code{input} slot that will be used to access the value.
+#' @param label Display label for the control, or \code{NULL} for no label.
+#' @param value Initial value(s), dates as character string are accepted in \code{yyyy-mm-dd} format, 
+#' or Date/POSIXct object. Can be a single value or severals.
+#' @param multiple Select multiple dates.
+#' @param range Select a date range.
+#' @param timepicker Add a timepicker below calendar to select time.
+#' @param separator Separator between dates when several are selected, default to \code{" - "}.
+#' @param placeholder A character string giving the user a hint as to what can be entered into the control.
+#' @param dateFormat Format to use to display date(s), default to \code{"yyyy-mm-dd"}
+#' @param minDate The minimum allowed date. Either a Date object, or a string in \code{yyyy-mm-dd} format.
+#' @param maxDate The maximum allowed date. Either a Date object, or a string in \code{yyyy-mm-dd} format.
+#' @param disabledDates A vector of dates to disable, e.g. won't be able to select one of dates passed.
+#' @param view Starting view, one of \code{'days'} (default), \code{'months'} or \code{'years'}.
+#' @param minView Minimal view, one of \code{'days'} (default), \code{'months'} or \code{'years'}.
+#' @param monthsField Names for the months when view is 'months',
+#'  use \code{'monthsShort'} for abbreviations or \code{'months'} for full names. 
+#' @param clearButton If \code{TRUE}, then button "Clear" will be visible.
+#' @param todayButton If \code{TRUE}, then button "Today" will be visible.
+#' @param autoClose If \code{TRUE}, then after date selection, datepicker will be closed.
+#' @param timepickerOpts Options for timepicker, see \link{timepickerOptions}.
+#' @param position Where calendar should appear, a two word string like 
+#'  \code{'bottom left'} (default), or \code{'top right'}, \code{'left top'}.
+#' @param update_on When to send selected value to server: on \code{'change'} 
+#'  or when calendar is \code{'close'}d.
+#' @param addon Display a calendar icon to \code{'right'} or the \code{'left} 
+#'  of the widget, or \code{'none'}.
+#' @param language Language to use, can be one of \code{'cs'}, \code{'da'},
+#'  \code{'de'}, \code{'en'}, \code{'es'}, \code{'fi'}, \code{'fr'}, 
+#'  \code{'hu'}, \code{'nl'}, \code{'pl'}, \code{'pt-BR'}, \\code{'pt'}, 
+#'  \code{'ro'}, \code{'sk'}, \code{'zh'}.
+#' @param inline If \code{TRUE}, datepicker will always be visible.
+#' @param width The width of the input, e.g. \code{'400px'}, or \code{'100%%'}; see \link{validateCssUnit}.
+#' 
+#' @note This widget prevents `dateInput` from working, don't use both !
+#'
+#' @export
+#' 
+#' @importFrom htmltools tags tagList validateCssUnit
+#' @importFrom shiny singleton
+#' @importFrom jsonlite toJSON
+#' @importFrom lubridate with_tz
+#'
+#' @examples
+#' 
 airDatepickerInput <- function(inputId, label = NULL, value = NULL, multiple = FALSE,
                                range = FALSE, timepicker = FALSE,
                                separator = " - ", placeholder = NULL, 
@@ -33,17 +84,17 @@ airDatepickerInput <- function(inputId, label = NULL, value = NULL, multiple = F
     several.ok = TRUE
   )
   if (inherits(value, "POSIXt")) {
-    value <- format(lubridate::with_tz(value, tzone = "UTC"), format = "%Y-%m-%dT%H:%M:00")
+    value <- format(with_tz(value, tzone = "UTC"), format = "%Y-%m-%dT%H:%M:00")
   }
   if (!is.null(disabledDates)) {
-    disabledDates <- jsonlite::toJSON(x = disabledDates, auto_unbox = FALSE)
+    disabledDates <- toJSON(x = disabledDates, auto_unbox = FALSE)
   }
   paramsAir <- dropNulls(list(
     id = inputId,
     class = "sw-air-picker",
     `data-language` = language, 
     `data-timepicker` = tolower(timepicker),
-    `data-start-date` = if (!is.null(value)) jsonlite::toJSON(x = value, auto_unbox = FALSE),
+    `data-start-date` = if (!is.null(value)) toJSON(x = value, auto_unbox = FALSE),
     `data-range` = tolower(range),
     `data-date-format` = dateFormat,
     `data-min-date` = minDate, `data-max-date` = maxDate,
@@ -99,7 +150,7 @@ airDatepickerInput <- function(inputId, label = NULL, value = NULL, multiple = F
     tags$div(
       class = "form-group shiny-input-container",
       style = if (!is.null(width)) 
-        paste0("width: ", htmltools::validateCssUnit(width), ";"),
+        paste0("width: ", validateCssUnit(width), ";"),
       if (!is.null(label)) tags$label(label, `for` = inputId),
       tagAir
     )
@@ -146,7 +197,7 @@ shiny::registerInputHandler("air.date", function(data, ...) {
 
 shiny::registerInputHandler("air.datetime", function(data, ...) {
   parse_datetime <- function(x) {
-    lubridate::with_tz(as.POSIXct(x = x, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC"))
+    with_tz(as.POSIXct(x = x, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC"))
   }
   if (is.null(data)) {
     NULL
@@ -165,6 +216,25 @@ shiny::registerInputHandler("air.datetime", function(data, ...) {
 
 
 
+
+#' Change the value of a airDate input on the client
+#'
+#' @param session The \code{session} object passed to function given to \code{shinyServer}.
+#' @param inputId The id of the input object.
+#' @param label The label to set for the input object.
+#' @param value The value to set for the input object.
+#' @param clear Logical, clear all previous selected dates.
+#'
+#' @export
+#' 
+#' @importFrom jsonlite toJSON
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' # todo
+#' 
+#' }
 updateAirDateInput <- function(session, inputId, label = NULL, value = NULL, clear = FALSE) {
   stopifnot(is.logical(clear))
   formatDate <- function(x) {
@@ -174,7 +244,7 @@ updateAirDateInput <- function(session, inputId, label = NULL, value = NULL, cle
   }
   value <- formatDate(value)
   if (!is.null(value)) {
-    value <- as.character(jsonlite::toJSON(x = value, auto_unbox = FALSE))
+    value <- as.character(toJSON(x = value, auto_unbox = FALSE))
   }
   message <- dropNulls(list(
     label = label,
